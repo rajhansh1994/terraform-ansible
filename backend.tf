5c191781522a526f23d69bcb895476eaf27a1598
Rajhansh <kumar.rajhansh@capgemini.com>
2021-10-28 09:21:37 +0530

    initial commit1

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "raj-anz-remote-state-backend"
-    key            = "global/ec2/terratest/terraform.tfstate"
+    key            = "global/ec2/terraform-ansible/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "raj-anz-remote-state-backend"
     encrypt        = true
